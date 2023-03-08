714dd03e80b3ba37fc4c229491197b49d5dfe7d1
OlexandrKhalamei <o.khalamei@betegy.com>
2023-03-08 18:13:35 +0200

    Add loader

--- src/components/Meals/MealList.js
+++ src/components/Meals/MealList.js
@@ -5,9 +5,11 @@ import MealItem from "./MealItem/MealItem";
 
 const MealList = () => {
   const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     const fetchMeals = async () => {
+      setIsLoading(true);
       const response = await fetch(
         "https://react-course-http-d5a27-default-rtdb.firebaseio.com/meals.json"
       );
@@ -23,9 +25,18 @@ const MealList = () => {
         });
       }
       setMeals(loadedMeals);
+      setIsLoading(false);
     };
     fetchMeals();
   }, []);
+
+  if (isLoading) {
+    return (
+      <section className={styles.loading}>
+        <p>Data extraction...</p>
+      </section>
+    );
+  }
 
   const mealList = meals.map((meal) => (
     <MealItem
